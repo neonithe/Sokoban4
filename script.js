b233c7408add1e60b04ef2df87441883ea4ff1b7
Mattias Andersson <neonithe@gmail.com>
2020-10-06 14:36:48 +0200

    All Done, Some fixes in HTML, still to do.

--- script.js
+++ script.js
@@ -10,26 +10,102 @@ var PRINT   = "X";
 var PLAYERIMG   = "<img src='img/playerR.png'>";
 var MONSTERIMG  = "<img src='img/monster.png'>";
 
+var MOVR = "<img src='img/playerR.png' width='47' height='47'>";
+var MOVL = "<img src='img/playerL.png' width='47' height='47'>";
+var MOVD = "<img src='img/playerD.png' width='47' height='47'>";
+var MOVU = "<img src='img/playerU.png' width='47' height='47'>";
+
 var gameMap = mapStart();
 var pPos = {i:0, j:0};
 
 // Timer
+var s = 0;
+var m = 0;
+var t = 0;
+
+// Get values for count, timer or if game is ended
+var countV = 0;
+var timeV = 0;
+var gEnd;
+
+// Modal
+var modal = document.getElementById("myModal");             // Get the modal
 
 /** Messages to HTML **/
-var count = document.getElementById("count");
+var count   = document.getElementById("count");
+var message = document.getElementById("message");
+var whatMap = document.getElementById("whatMap");
+var gTimer  = document.getElementById("timeCount");
+/** HTML **/
+var printBoard = document.getElementById("gameMap");
+var modalHtml  = document.getElementById("modalHtml");
+
+/** Start Page **/
+function startPage() {
+    stop();
+    reset();
+    count.innerHTML = 0;
+    countV = 0;
+    whatMap.innerHTML = "START PAGE";
+    message.innerHTML = "Click one of the maps to start the game";
+    gameMap = convertMap(mapStart());
+    createBoard();
+}
+
+startPage();
 
 /** Game Menu **/
 function startGame(map) {
     switch ( map ) {
         case 1:
+            startTable(map);
             gameMap = convertMap(map1());
-            console.log(gameMap);
-            createBoard();
+            getPlayer(map1());
+        break;
+        case 2:
+            startTable(map);
+            gameMap = convertMap(map2());
+            getPlayer(map2());
         break;
     }
 }
 
-/** Move player with game rules [WALL] [MOVE MONSTER] **/
+function startTable(nr) {
+    stop();
+    reset();
+    count.innerHTML = 0;
+    message.innerHTML = "Click one of the maps to start the game";
+    whatMap.innerHTML = "Map "+nr;
+    timer();
+}
+function endTable() {
+    stop();
+    pPos.i = 0;
+    pPos.j = 0;
+
+}
+
+/** TIMER **/
+function runTime() {
+    s++;
+    if ( s >= 60 ) { s = 0; m++; }
+    timeV = (m > 9 ? m : "0" + m) + ":" + (s > 9 ? s : "0" + s);
+    gTimer.innerHTML = timeV;
+    timer();
+}
+function timer() {
+    t = setTimeout(runTime, 1000);
+}
+function stop() {
+    clearTimeout(t);
+}
+function reset() {
+    stop();
+    gTimer.textContent ="00:00";
+    s = 0; m = 0;
+}
+
+/** Move player with game rules **/
 function move(i,j) {
 
     var iD = i - pPos.i;
@@ -43,9 +119,7 @@ function move(i,j) {
                  gameMap[i][j].status = null;
                  gameMap[i+iD][j+jD].status = MONSTER;
                 canMove = true;
-            } else {
-                canMove = false;
-            }
+            } else { canMove = false;}
         }
         if ( canMove )  updateCheckPlayer(i,j);
     }
@@ -62,7 +136,8 @@ function updateCheckPlayer(i,j) {
     gameMap[pPos.i][pPos.j].status = PLAYER;
 
     /** Update move counter **/
-    count.innerHTML++;
+    countV++;
+    count.innerHTML = countV;
 
     /** Update map **/
     createBoard();
@@ -75,7 +150,9 @@ function updateCheckPlayer(i,j) {
         }
     }
     if ( inGame ) {
-        message.innerHTML = "You are a winner";
+        openModal("<p>YOU ARE A WINNER</p><br><p>Moves:"+countV+" || Timer: "+timeV+"</p>");
+        endTable();
+        gEnd = true;
     }
 }
 
@@ -88,15 +165,19 @@ window.addEventListener("keydown", function(event) {
     }
     switch ( event.keyCode ) {
         case 37: // Vänster
+            PLAYERIMG = MOVL;
             move(i, j-1);
             break;
         case 38: // Upp
+            PLAYERIMG = MOVU;
             move(i-1, j);
             break;
         case 39: // Höger
+            PLAYERIMG = MOVR;
             move(i, j+1);
             break;
         case 40: // Ner
+            PLAYERIMG = MOVD;
             move(i+1, j);
             break;
         case 32: // Ner
@@ -119,13 +200,31 @@ window.addEventListener("keydown", function(event) {
     }
 }, false);
 
+/** Modal window ********************************************/
+function closeModal() {
+    if ( gEnd ) {
+        startPage();
+    }
+    modal.style.display = "none";
+}
+function openModal(message) {
+    modalHtml.innerHTML = message;
+    modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 
 /**  1. Create HTML, 2. Player position, 3. Maps, 4. Convert Map   ***********************************************/
 
 /** 1. Create HTML table **/
 function createBoard() {
-    var printBoard = document.getElementById("gameMap");
     var html    = "";
     var cell    = "";
     var cellCss = "";
@@ -171,29 +270,14 @@ function getPlayer(array) {
             }
         }
     }
+    createBoard();
 }
-console.log(gameMap);
-console.log(goalMap(mapStart()));
 
-/** 3. MAPS **/
+/** 3. MAPS     M = Monster | P = PLayer | W = Wall | F = Floor | G = Goal      **/
 function map1() {
 
     var map = [];
 
-    /**     M = Monster | P = PLayer | W = Wall | F = Floor | G = Goal      **/
-    /*
-        map[0]   = ["E", "E", "E", "E", "W", "W", "W", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
-        map[1]   = ["E", "E", "E", "E", "W", "F", "F", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
-        map[2]   = ["E", "E", "E", "E", "W", "M", "F", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
-        map[3]   = ["E", "E", "W", "W", "W", "F", "F", "M", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
-        map[4]   = ["E", "E", "W", "F", "F", "M", "F", "M", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
-        map[5]   = ["W", "W", "W", "F", "W", "F", "W", "W", "F", "W", "E", "E", "W", "W", "W", "W", "W", "W", "E"];
-        map[6]   = ["W", "F", "F", "F", "W", "F", "W", "W", "F", "W", "W", "W", "W", "F", "F", "G", "G", "W", "E"];
-        map[7]   = ["W", "F", "M", "F", "F", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "G", "G", "W", "E"];
-        map[8]   = ["W", "W", "W", "W", "W", "F", "W", "W", "W", "F", "W", "P", "W", "F", "F", "G", "G", "W", "E"];
-        map[9]   = ["E", "E", "E", "E", "W", "F", "F", "F", "F", "F", "W", "W", "W", "W", "W", "W", "W", "W", "E"];
-        map[10]  = ["E", "E", "E", "E", "W", "W", "W", "W", "W", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E"];
-    */
     map[0]   = ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"];
     map[1]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
     map[2]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
@@ -209,18 +293,36 @@ function map1() {
     return map;
 }
 
+function map2() {
+    var map = [];
+
+    map[0]   = ["E", "E", "E", "E", "W", "W", "W", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
+    map[1]   = ["E", "E", "E", "E", "W", "F", "F", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
+    map[2]   = ["E", "E", "E", "E", "W", "M", "F", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
+    map[3]   = ["E", "E", "W", "W", "W", "F", "F", "M", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
+    map[4]   = ["E", "E", "W", "F", "F", "M", "F", "M", "F", "W", "E", "E", "E", "E", "E", "E", "E", "E", "E"];
+    map[5]   = ["W", "W", "W", "F", "W", "F", "W", "W", "F", "W", "E", "E", "W", "W", "W", "W", "W", "W", "E"];
+    map[6]   = ["W", "F", "F", "F", "W", "F", "W", "W", "F", "W", "W", "W", "W", "F", "F", "G", "G", "W", "E"];
+    map[7]   = ["W", "F", "M", "F", "F", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "G", "G", "W", "E"];
+    map[8]   = ["W", "W", "W", "W", "W", "F", "W", "W", "W", "F", "W", "P", "W", "F", "F", "G", "G", "W", "E"];
+    map[9]   = ["E", "E", "E", "E", "W", "F", "F", "F", "F", "F", "W", "W", "W", "W", "W", "W", "W", "W", "E"];
+    map[10]  = ["E", "E", "E", "E", "W", "W", "W", "W", "W", "W", "W", "E", "E", "E", "E", "E", "E", "E", "E"];
+
+    return map;
+}
+
 function mapStart() {
     var map = [];
 
-    map[0]   = ["G", "G", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"];
+    map[0]   = ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"];
     map[1]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[2]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[3]   = ["W", "F", "F", "F", "F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[4]   = ["W", "F", "F", "F", "F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[5]   = ["W", "F", "F", "F", "F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[6]   = ["W", "F", "F", "F", "F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[7]   = ["W", "F", "F", "F", "F", "X", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
-    map[8]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
+    map[2]   = ["W", "F", "W", "W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W", "W", "F", "W"];
+    map[3]   = ["W", "F", "W", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "M", "W", "F", "W"];
+    map[4]   = ["W", "F", "W", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "M", "W", "F", "W"];
+    map[5]   = ["W", "F", "W", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "M", "W", "F", "W"];
+    map[6]   = ["W", "F", "W", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "M", "W", "F", "W"];
+    map[7]   = ["W", "F", "W", "M", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "M", "W", "F", "W"];
+    map[8]   = ["W", "F", "W", "W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W", "W", "F", "W"];
     map[9]   = ["W", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "W"];
     map[10]  = ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W"];
 
@@ -247,11 +349,11 @@ function convertMap(array) {
                 array[i][j] = {model: FLOOR, status: MONSTER};
             } else if ( array[i][j] === "P" ) {
                 array[i][j] = {model: FLOOR, status: PLAYER};
-                getPlayer(map1());
             }
         }
     }
   return array;
 }
+
 
 /** Load map from file **/
